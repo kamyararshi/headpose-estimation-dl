db3a55ef7ac03db41d38c0c2fc598347c3ba5b5a
kamyar arshi <37985246+kamyararshi@users.noreply.github.com>
2023-07-18 17:22:10 +0000

    utils.py dataset.py added

---
+++ codes/dataset.py
@@ -0,0 +1,131 @@
+import os
+import numpy as np
+import cv2
+import pandas as pd
+
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from PIL import Image, ImageFilter
+
+from codes import utils
+
+FOLDER_NAMES = ['AFW', 'AFW_Flip', 'HELEN', 'HELEN_Flip', 'IBUG', 'IBUG_Flip', 'LFPW', 'LFPW_Flip']
+
+def get_list_from_filenames(data_dir, folder_names, test_dict):
+    """
+    """
+    filenames_dict=[]
+
+    for folder in folder_names:
+        folder_path = os.path.join(data_dir, folder)
+        all_files = os.listdir(folder_path)
+        filenames_dict += list(set([os.path.join(folder, os.path.splitext(i)[0]) for i in sorted(all_files) if os.path.join(folder, os.path.splitext(i)[0]) not in test_dict]))
+
+    return filenames_dict
+
+
+class Pose_300W_LP(Dataset):
+    # Head pose from 300W-LP dataset
+    def __init__(self,
+                data_dir,
+                transform=None,
+                img_ext='.jpg',
+                annot_ext='.mat',
+                image_mode='RGB',
+                test=False):
+        """Dataset class for the 300W_LP dataset, head pose estimation
+        Inputs:
+            - data_dir:
+            - transform:
+            - img_ext:
+            - annot_ext:
+            - image_mode:
+            - test:
+        """
+        self.data_dir = data_dir
+        self.transform = transform
+        self.img_ext = img_ext
+        self.annot_ext = annot_ext
+
+        TEST_SET = [os.path.join('AFW', f'AFW_9545523490_1_{i}') for i in range(18)] + [os.path.join('AFW', f'AFW_9545523490_2_{i}') for i in range(11)]\
+                    +[os.path.join('HELEN', f'HELEN_232194_1_{i}') for i in range(18)] + [os.path.join('HELEN', f'HELEN_1629243_1_{i}') for i in range(18)]\
+                    +[os.path.join('IBUG', f'IBUG_image_003_1_{i}') for i in range(11)] + [os.path.join('IBUG', f'IBUG_image_004_1_{i}') for i in range(12)]\
+                    +[os.path.join('LFPW', f'LFPW_image_test_0001_{i}') for i in range(16)] + [os.path.join('LFPW', f'LFPW_image_test_0002_{i}') for i in range(17)]\
+                    +[os.path.join('AFW_Flip', f'AFW_9545523490_1_{i}') for i in range(18)] + [os.path.join('AFW', f'AFW_9545523490_2_{i}') for i in range(11)]\
+                    +[os.path.join('HELEN_Flip', f'HELEN_232194_1_{i}') for i in range(18)] + [os.path.join('HELEN', f'HELEN_1629243_1_{i}') for i in range(18)]\
+                    +[os.path.join('IBUG_Flip', f'IBUG_image_003_1_{i}') for i in range(11)] + [os.path.join('IBUG', f'IBUG_image_004_1_{i}') for i in range(12)]\
+                    +[os.path.join('LFPW_Flip', f'LFPW_image_test_0001_{i}') for i in range(16)] + [os.path.join('LFPW', f'LFPW_image_test_0002_{i}') for i in range(17)]
+        
+        # Train-Test split
+        self.image_mode = image_mode
+        if not test:
+            self.data = get_list_from_filenames(self.data_dir, FOLDER_NAMES, TEST_SET)
+            self.length = len(self.data)
+        else:
+            self.data = TEST_SET
+            self.length = len(self.data)
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.data_dir, self.data[index] + self.img_ext))
+        img = img.convert(self.image_mode)
+        mat_path = os.path.join(self.data_dir, self.data[index] + self.annot_ext)
+
+        # Crop the face loosely
+        pt2d = utils.get_pt2d_from_mat(mat_path)
+        x_min = min(pt2d[0,:])
+        y_min = min(pt2d[1,:])
+        x_max = max(pt2d[0,:])
+        y_max = max(pt2d[1,:])
+
+        # k = 0.2 to 0.40
+        k = np.random.random_sample() * 0.2 + 0.2
+        x_min -= 0.6 * k * abs(x_max - x_min)
+        y_min -= 2 * k * abs(y_max - y_min)
+        x_max += 0.6 * k * abs(x_max - x_min)
+        y_max += 0.6 * k * abs(y_max - y_min)
+        img = img.crop((int(x_min), int(y_min), int(x_max), int(y_max)))
+
+        # We get the pose in radians
+        pose = utils.get_ypr_from_mat(mat_path)
+        # And convert to degrees.
+        pitch = pose[0] * 180 / np.pi
+        yaw = pose[1] * 180 / np.pi
+        roll = pose[2] * 180 / np.pi
+
+        # Flip?
+        rnd = np.random.random_sample()
+        if rnd < 0.5:
+            yaw = -yaw
+            roll = -roll
+            img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+        # Blur?
+        rnd = np.random.random_sample()
+        if rnd < 0.05:
+            img = img.filter(ImageFilter.BLUR)
+        # Make img tensor
+        PILtoTensor = transforms.ToTensor()
+        img = PILtoTensor(img)
+
+        # Bin values
+        bins = np.array(range(-99, 102, 3))
+        binned_pose = np.digitize([yaw, pitch, roll], bins) - 1
+
+        # Get target tensors
+        labels = binned_pose
+        cont_labels = torch.FloatTensor([yaw, pitch, roll])
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, labels, cont_labels, self.data[index]
+
+    def __len__(self):
+        # 122,450
+        return self.length
+    
+    def _return_dataloader(self, batch_size, shuffle, num_workers=0):
+        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
---
+++ codes/motion_encoder.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+"""
+class ResBlock(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+    
+class ResNet18Sub(nn.Module):
+    def __init__(self, in_channels, pretrained=False):
+        super(ResNet18Sub, self).__init__()
+        self.pretrained = pretrained
+
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(64, 2)
+        self.layer2 = self._make_layer(128, 2, stride=2)
+        self.layer3 = self._make_layer(256, 2, stride=2)
+        self.layer4 = self._make_layer(512, 2, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        #self.fc = nn.Linear(512, num_classes)
+
+        if self.pretrained:
+            self.model = models.resnet18(weights=True)
+
+
+    def _make_layer(self, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or planes != 64:
+            downsample = nn.Sequential(
+                nn.Conv2d(64, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes),
+            )
+
+        layers = []
+        layers.append(ResBlock(64, planes, stride, downsample))
+        for i in range(1, blocks):
+            layers.append(ResBlock(planes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.pretrained:
+            return self.model(x)
+        else:
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+            x = self.layer1(x)
+            x = self.layer2(x)
+            x = self.layer3(x)
+            x = self.layer4(x)
+            x = self.avgpool(x)
+            #x = x.view(x.size(0), -1)
+            #x = self.fc(x)
+
+            return x
+"""        
+class ResNet18(nn.Module):
+    def __init__(self, in_channels, pretrained=True):
+        super(ResNet18, self).__init__()
+        # If we want pretrained weights or not
+        if pretrained:
+            self.model = models.resnet18(weights=pretrained)
+        else:
+            self.model = models.resnet18(weights=False)
+        # in_channels    
+        if in_channels != 3:
+            self.model.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+        # Set last fully connected layer to output 9 numbers;
+        # 3 for rotation, 3 for translation and 3 for scale (also needed to build translation matrix afterwards)
+        self.model.fc = nn.Linear(512, 9)
+
+    def forward(self, x):
+        return self.model(x)
