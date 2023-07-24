c82b90d84d675d09198f248a811ba39f29f1288f
kamyar arshi <37985246+kamyararshi@users.noreply.github.com>
2023-07-24 11:11:10 +0000

    training with single lr, tensorboard added

--- train.py
+++ train.py
@@ -3,7 +3,9 @@ import torch.nn as nn
 from torch.optim import Adam
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torchvision import transforms 
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+
 
 from codes import dataset, motion_encoder, utils
 
@@ -12,6 +14,10 @@ import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 import tqdm
 import logging
+from time import gmtime, strftime
+from shutil import copy
+import os
+
 
 #TODO: Delete this later
 import warnings
@@ -24,7 +30,9 @@ def main():
     parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
             default=0, type=int)
     parser.add_argument('--config', dest='config', help='Path to config file.',
-          default='configs/pose300wlp.yml', type=str)
+          default='./configs/pose300wlp.yml', type=str)
+    parser.add_argument('--log_dir', dest='log_dir', help='Path to logs and saved checkpoints.',
+          default='./logs/', type=str)
     
     args = parser.parse_args()
 
@@ -33,6 +41,15 @@ def main():
 
     with open(args.config) as f:
         configs = yaml.safe_load(f)
+
+    # Log Dir
+    log_dir = os.path.join(args.log_dir, os.path.basename(args.config).split('.')[0])
+    log_dir += ' ' + strftime("%d_%m_%y_%H.%M.%S", gmtime())
+    
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    if not os.path.exists(os.path.join(log_dir, os.path.basename(args.config))):
+        copy(args.config, log_dir)
 
     # Image Transformations
     transformations = transforms.Compose([
@@ -69,11 +86,14 @@ def main():
     criterion = nn.MSELoss().to(device=device)
     optimizer = Adam(model.parameters(), lr=configs['train_params']['lr'])
 
+    #TODO: Add MultiStepLR later
+
     # Training Loop
     model.train()
+    writer = SummaryWriter(log_dir=log_dir)
     train_loss=[]
     for epoch in tqdm.trange(configs['train_params']['num_epochs']):
-      for batch in tqdm.tqdm(train_loader):
+        for batch in tqdm.tqdm(train_loader):
             optimizer.zero_grad()
 
             img, _, labels, _ = batch
@@ -85,24 +105,44 @@ def main():
             loss.backward()
             optimizer.step()
       
-      train_loss.append(loss.detach().item())
-
-    # Eval
-    model.eval()
-    eval_loss=[]
-    with torch.no_grad():
-      i=0
-      for i, batch in enumerate(tqdm.tqdm(test_loader)):
-            img, _, labels, _ = batch
-            img = img.to(device)
-            labels = labels.to(device)
-            pose = model(img)
-            eval_loss.append(criterion(pose, labels).item())
+        train_loss.append(loss.detach().item())
+        writer.add_scalar("Train Loss", loss.detach().item(), epoch)
 
 
+        # Eval
+        model.eval()
+        eval_loss=[]
+        with torch.no_grad():
+            i=0
+            for i, batch in enumerate(test_loader):
+                img, _, labels, _ = batch
+                img = img.to(device)
+                labels = labels.to(device)
+                pose = model(img)
+                eval_loss.append(criterion(pose, labels).item())
+            
+            # Plot the image and pose using the 'draw_axis' function
+            pitch, yaw, roll, tdx, tdy, tdz, _ = pose[0].cpu()
+            image_to_plot = utils.draw_axis(img[:1,...], yaw, pitch, roll, tdx, tdy)
+            image_to_plot_tensor = torch.from_numpy(image_to_plot).permute(2,0,1)
 
+            # Add the image to TensorBoard
+            writer.add_image("Example Image", image_to_plot_tensor, global_step=epoch)
+        
+        # Logging the evaluation loss to TensorBoard
+        avg_eval_loss = sum(eval_loss) / len(eval_loss)
+        writer.add_scalar("Eval Loss", avg_eval_loss, epoch)
 
-    return train_loss, eval_loss
+    writer.close()
+
+    # Save model
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': train_loss[-1],
+            }, os.path.join(log_dir, f'{epoch+1}.pth.tar'))
+
 
 if __name__ == '__main__':
     
