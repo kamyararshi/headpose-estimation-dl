e480d6f504b1319f452528a4ccc0060ff9dddabe
kamyar arshi <37985246+kamyararshi@users.noreply.github.com>
2023-07-24 11:10:31 +0000

    draw axis func added

--- codes/utils.py
+++ codes/utils.py
@@ -117,3 +117,44 @@ def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size = 100):
     cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(255,0,0),2)
 
     return img
+
+
+def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size = 100):
+    """draws axis w.r.t the angles
+    img: tensor of shape (1, C, H, W) or numpy image of hsape H,W,C
+    """
+
+    if len(img.shape) == 4 and isinstance(img, torch.Tensor):
+        #Tensor to numpy shape H,W,C
+        img = img.permute(0, 2, 3, 1).contiguous().cpu().squeeze(0).numpy()
+
+    pitch = pitch * np.pi / 180
+    yaw = -(yaw * np.pi / 180)
+    roll = roll * np.pi / 180
+
+    if tdx != None and tdy != None:
+        tdx = tdx
+        tdy = tdy
+    else:
+        height, width = img.shape[:2]
+        tdx = width / 2
+        tdy = height / 2
+
+    # X-Axis pointing to right. drawn in red
+    x1 = size * (cos(yaw) * cos(roll)) + tdx
+    y1 = size * (cos(pitch) * sin(roll) + cos(roll) * sin(pitch) * sin(yaw)) + tdy
+
+    # Y-Axis | drawn in green
+    #        v
+    x2 = size * (-cos(yaw) * sin(roll)) + tdx
+    y2 = size * (cos(pitch) * cos(roll) - sin(pitch) * sin(yaw) * sin(roll)) + tdy
+
+    # Z-Axis (out of the screen) drawn in blue
+    x3 = size * (sin(yaw)) + tdx
+    y3 = size * (-cos(yaw) * sin(pitch)) + tdy
+
+    cv2.line(img, (int(tdx), int(tdy)), (int(x1),int(y1)),(0,0,255),4)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x2),int(y2)),(0,255,0),4)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(255,0,0),4)
+
+    return img
